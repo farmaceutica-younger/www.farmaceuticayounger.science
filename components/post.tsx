4144c5516b851c478720e5d6d5d03dcb46f07a26
Ludovico Russo <ludus.russo@gmail.com>
2022-05-15 21:46:01 +0200

    fix: update gui

--- components/post.tsx
+++ components/post.tsx
@@ -43,10 +43,10 @@ const components: any = {
     <YouTube className="w-full" opts={{}} videoId={videoId} />
   ),
   blockquote: (props: any) => (
-    <div className="not-prose relative bg-gray-100 px-4 py-8">
-      <blockquote className=" md:flex md:flex-grow md:flex-col">
+    <div className="not-prose relative border-4 border-pink-600 px-4 py-8">
+      <blockquote className="md:flex md:flex-grow md:flex-col">
         <svg
-          className="absolute top-4 left-4 h-8 w-8 -translate-x-3 -translate-y-2 transform  text-pink-400"
+          className="absolute -top-4 -left-4 m-2 h-8 w-8 transform bg-gray-50 text-pink-600"
           fill="currentColor"
           viewBox="0 0 32 32"
           aria-hidden="true"
@@ -54,14 +54,14 @@ const components: any = {
           <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
         </svg>
         <svg
-          className="absolute right-4 -bottom-2 h-8 w-8 -translate-x-3 -translate-y-2 rotate-180 transform text-pink-400"
+          className="absolute -right-4 -bottom-4 h-8 w-8 rotate-180 transform bg-gray-50 text-pink-600"
           fill="currentColor"
           viewBox="0 0 32 32"
           aria-hidden="true"
         >
           <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
         </svg>
-        <div className="relative text-xl font-medium italic md:flex-grow">
+        <div className="relative px-2 text-center text-xl font-medium italic md:flex-grow">
           <p className="relative">{props.children}</p>
         </div>
       </blockquote>
