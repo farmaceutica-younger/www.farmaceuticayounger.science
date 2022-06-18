350b958343b073d92011277eb9c3e14da7ff3a29
Ludovico Russo <ludus.russo@gmail.com>
2022-06-18 15:16:19 +0200

    update

--- utils/cloudinary-url.tsx
+++ utils/cloudinary-url.tsx
@@ -3,15 +3,12 @@ export const cloudinaryUrl = (link: string) => {
     /https?:\/\/res.cloudinary.com\/.*\/image\/upload\/v[^\/]*/,
     ""
   );
-  console.log(link, res);
   return res;
 };
 
 const rg = /https?:\/\/res.cloudinary.com\/(.*)\/image\/upload\/v[^\/]*(.*)/;
 export const resizeCloudinaryImage = (link: string, w = 300) => {
-  console.log("ciao");
   return link.replace(rg, (_, name, id) => {
-    console.log(name, id);
     return `https://res.cloudinary.com/${name}/image/upload/c_scale,w_${w},f_auto${id}`;
   });
 };
