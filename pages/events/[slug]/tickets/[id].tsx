cef2256a0926420c24e0e43bcda97bb1d4756b7c
Ludovico Russo <ludus.russo@gmail.com>
2022-07-08 13:39:36 +0200

    create image on demand

--- pages/events/[slug]/tickets/[id].tsx
+++ pages/events/[slug]/tickets/[id].tsx
@@ -10,19 +10,21 @@ import Tilt from "react-parallax-tilt";
 import { db } from "services/db";
 import { getEventDate } from "utils/dates";
 import { readTime } from "utils/readTime";
+import p from "puppeteer";
+import { cloudinary, cloudinaryConfig } from "services/cloudinary";
 
 export const ShowTicketPage = ({
   ticket,
   frontmatter,
   linkedinUrl,
+  imageTicket,
 }: Awaited<ReturnType<typeof getProps>>["props"]) => {
-  const image = `/api/tickets/ticket?eventId=${frontmatter.id}&ticketId=${ticket.id}`;
   return (
     <>
       <SEO
         title={`Ticket di ${ticket.name}`}
         description={`${ticket.name} parteciperà a all&apos;evento ${frontmatter.title} di Farmaceutica Younger`}
-        image={image}
+        image={imageTicket}
         author={frontmatter.author.name}
         date={frontmatter.createdAt}
         type="article"
@@ -122,6 +124,8 @@ const getProps = async (
     });
   } catch {}
 
+  const imageTicket = await getTicketImagePreview(event, ticket);
+
   return {
     props: {
       source: mdxSource,
@@ -131,6 +135,133 @@ const getProps = async (
       },
       ticket,
       linkedinUrl: getLinkedinOAUTHUrl(event.id),
+      imageTicket,
     },
   };
 };
+
+const getTicketImagePreview = async (
+  event: Event & {
+    author: Author;
+  },
+  ticket: EventTicket
+) => {
+  const imagePublicId = `events/${event.id}/tickets/${ticket.id}`;
+  const exists = await checkIfimageExists(imagePublicId);
+
+  if (!exists) {
+    const s = await createScreen(event, ticket);
+    await new Promise<void>((resolve, reject) => {
+      const str = cloudinary.uploader.upload_stream(
+        {
+          public_id: imagePublicId,
+        },
+        (err, r) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          }
+          resolve();
+        }
+      );
+      str.end(s);
+    });
+  }
+
+  return `https://res.cloudinary.com/${cloudinaryConfig.cloudName}/image/upload/${imagePublicId}`;
+};
+
+const checkIfimageExists = async (id: string) => {
+  try {
+    const resImage: any = await cloudinary.api.resource(id);
+    return !resImage.error;
+  } catch (e) {
+    return false;
+  }
+};
+
+interface Query {
+  date: string;
+  location: string;
+  eventName: string;
+  avatar: string;
+  name: string;
+  role: string;
+}
+
+const createScreen = async (event: Event, ticket: EventTicket) => {
+  const browser = await p.launch({
+    args: minArgs,
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  const q: Query = {
+    date: getEventDate(event.startDate, event.endDate),
+    avatar: ticket.avatar,
+    eventName: event.title,
+    location: event.location,
+    name: ticket.name,
+    role: ticket.role,
+  };
+
+  await page.setViewport({
+    width: 1000,
+    height: 500,
+  });
+
+  const url = new URL("_i/ticket", "http://localhost:3000");
+  for (const [key, value] of Object.entries(q)) {
+    url.searchParams.append(key, value);
+  }
+
+  await page.goto(url.href);
+
+  const res = await page.screenshot({
+    encoding: "binary",
+    type: "png",
+    omitBackground: true,
+  });
+
+  await browser.close();
+  return res as Buffer;
+};
+
+const minArgs = [
+  "--autoplay-policy=user-gesture-required",
+  "--disable-background-networking",
+  "--disable-background-timer-throttling",
+  "--disable-backgrounding-occluded-windows",
+  "--disable-breakpad",
+  "--disable-client-side-phishing-detection",
+  "--disable-component-update",
+  "--disable-default-apps",
+  "--disable-dev-shm-usage",
+  "--disable-domain-reliability",
+  "--disable-extensions",
+  "--disable-features=AudioServiceOutOfProcess",
+  "--disable-hang-monitor",
+  "--disable-ipc-flooding-protection",
+  "--disable-notifications",
+  "--disable-offer-store-unmasked-wallet-cards",
+  "--disable-popup-blocking",
+  "--disable-print-preview",
+  "--disable-prompt-on-repost",
+  "--disable-renderer-backgrounding",
+  "--disable-setuid-sandbox",
+  "--disable-speech-api",
+  "--disable-sync",
+  "--hide-scrollbars",
+  "--ignore-gpu-blacklist",
+  "--metrics-recording-only",
+  "--mute-audio",
+  "--no-default-browser-check",
+  "--no-first-run",
+  "--no-pings",
+  "--no-sandbox",
+  "--no-zygote",
+  "--password-store=basic",
+  "--use-gl=swiftshader",
+  "--use-mock-keychain",
+  "--disable-web-security",
+];
