b9ebe30109190102acb8e60eb9d570e93ba515db
Ludovico Russo <ludus.russo@gmail.com>
2022-07-09 12:09:43 +0200

    update

--- pages/events/[slug]/register.tsx
+++ pages/events/[slug]/register.tsx
@@ -2,7 +2,7 @@ import { Event } from "@prisma/client";
 import { Footer } from "components/footer";
 import { Header } from "components/header";
 import { getLinkedinOAUTHUrl } from "config/linkedin";
-import { GetServerSidePropsContext, GetStaticPropsContext } from "next";
+import { GetServerSidePropsContext } from "next";
 import { db } from "services/db";
 
 const RegisterPage = ({
@@ -16,26 +16,26 @@ const RegisterPage = ({
     <>
       <Header />
       <div className="prose m-auto mt-10 max-w-md">
-        <h2>Registrati all'evento {frontmatter.title}</h2>
+        <h2>Registrati all&apos;evento {frontmatter.title}</h2>
         <p>
           Segui questi 4 semplici step per ottenere il tuo personalissimo Ticket
-          di accesso all'evento:
+          di accesso all&apos;evento:
         </p>
         <ol>
           <li>
             Clicca sul bottone{" "}
-            <strong>"Ottieni il Ticket con Linkedin!"</strong>
+            <strong>&quot;Ottieni il Ticket con Linkedin!&quot;</strong>
           </li>
           <li>
             Accedi al tuo profilo di <strong>Linkedin</strong>
           </li>
           <li>
             Ti verrà generato in automatico il tuo <strong>Ticket</strong> per
-            accedere all'evento
+            accedere all&apos;evento
           </li>
           <li>
             <strong>Condividi</strong> il ticket con la tua rete di Contatti per
-            far sapere a tutti che partecipera all'evento
+            far sapere a tutti che partecipera all&apos;evento
           </li>
         </ol>
         <div className="flex">
